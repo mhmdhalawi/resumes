ae304092956ac6d506b35c2f33c34f898191390c
mhmdhalawi <mohammadhalawi93@gmail.com>
2024-11-01 12:36:50 +0200

    feat: update ActivityLogs model with new fields and enums, remove description from Education model, and make skill level optional

---
+++ prisma/migrations/20241101100727_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `educations` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "educations" DROP COLUMN "description";
---
+++ prisma/migrations/20241101101755_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "skills" ALTER COLUMN "level" DROP NOT NULL;
